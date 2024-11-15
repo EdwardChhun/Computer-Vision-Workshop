d78ebb00fcd0d36f4757b36e2416c45760acfb10
Edward Chhun <edwardchhun3@gmail.com>
2024-11-15 03:11:01 -0800

    hot fix

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 from roboflow import Roboflow
 
-rf = Roboflow(api_key="r7qSIBlL0ryPm5qonSPH")
+rf = Roboflow(api_key="YOUR_API_KEY")
 project = rf.workspace("roboflow-58fyf").project("rock-paper-scissors-sxsw")
 version = project.version(14)
 dataset = version.download("yolov11")
